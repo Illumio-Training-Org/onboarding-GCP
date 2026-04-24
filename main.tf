5d509475b6a13d5a3c382c0c838176bf089d1bc0
sp11ker <71824282+sp11ker@users.noreply.github.com>
2026-04-24 15:31:45 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,34 +4,66 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 5.0"
     }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
   }
 }
+
+###############################
+# Variables
+###############################
 
 variable "gcp_project" {
   type = string
 }
 
-provider "google" {
-  project = var.gcp_project
-  region  = "europe-west2"
-  zone    = "europe-west2-a"
+variable "gcp_region" {
+  default = "europe-west2"
 }
 
-resource "tls_private_key" "ssh_key" {
+variable "gcp_zone" {
+  default = "europe-west2-a"
+}
+
+###############################
+# Provider
+###############################
+
+provider "google" {
+  project = var.gcp_project
+  region  = var.gcp_region
+  zone    = var.gcp_zone
+}
+
+###############################
+# SSH Key
+###############################
+
+resource "tls_private_key" "ssh" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
 resource "local_file" "private_key" {
-  content         = tls_private_key.ssh_key.private_key_pem
-  filename        = "${path.module}/my-keypair.pem"
+  content         = tls_private_key.ssh.private_key_pem
+  filename        = "${path.module}/id_rsa"
   file_permission = "0400"
 }
+
+###############################
+# VM (Default VPC only)
+###############################
 
 resource "google_compute_instance" "vm" {
   name         = "simple-vm"
   machine_type = "e2-micro"
-  zone         = "europe-west2-a"
+  zone         = var.gcp_zone
 
   boot_disk {
     initialize_params {
@@ -41,18 +73,23 @@ resource "google_compute_instance" "vm" {
 
   network_interface {
     network = "default"
-    access_config {}
+
+    access_config {} # gives public IP
   }
 
   metadata = {
-    ssh-keys = "debian:${tls_private_key.ssh_key.public_key_openssh}"
+    ssh-keys = "debian:${tls_private_key.ssh.public_key_openssh}"
   }
 }
+
+###############################
+# Outputs
+###############################
 
 output "public_ip" {
   value = google_compute_instance.vm.network_interface[0].access_config[0].nat_ip
 }
 
 output "ssh_command" {
-  value = "ssh -i my-keypair.pem debian@${google_compute_instance.vm.network_interface[0].access_config[0].nat_ip}"
+  value = "ssh -i id_rsa debian@${google_compute_instance.vm.network_interface[0].access_config[0].nat_ip}"
 }
